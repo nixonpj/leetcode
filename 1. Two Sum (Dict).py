27361062392dae468fb5397201368daf649afd5c
Nixon <nixon.pj93@gmail.com>
2020-11-14 12:20:31 -0600

    Solved "1. Two Sum" using a dictionary method. Much more memory usage.

--- 1. Two Sum (Dict).py
+++ 1. Two Sum (Dict).py
@@ -4,17 +4,18 @@ def twoSum(nums, target):
     :type target: int
     :rtype: List[int]
     """
-    d = dict(nums)
-    for index_1, num_1 in d.items():
-        num_2 = target - num_1
-        if num_2 in nums[index_1 + 1:]:
-            pass
+    d = {}
+    for i, num in enumerate(nums):
+        num_2 = target - num
+        if num_2 in d:
+            return [d[num_2], i]
+        else:
+            d[num] = i
 
 
-# print(twoSum([1, 2, 3, 4, 9], 10) == [0, 4])
-# print(twoSum([1, 3, 3], 6) == [1, 2])
-# print(twoSum([1, 2, 3, 4], 6) == [1, 3])
+print(twoSum([1, 2, 3, 4, 9], 10) == [0, 4])
+print(twoSum([1, 3, 3], 6) == [1, 2])
+print(twoSum([1, 2, 3, 4], 6) == [1, 3])
 
-a = [1,2,3]
-print(dict(a))
+
 
