3048acbdfb0e8c6ce1af2916773ab1d4f95e7646
Nixon <nixon.pj93@gmail.com>
2021-05-16 10:27:02 -0500

    Solved one hard problem

--- Longest Consecutive Sequence.py
+++ Longest Consecutive Sequence.py
@@ -1,43 +1,25 @@
 """
 Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
-Status: Incomplete. Come back to it later. Using hasttables/set is the correct approach but logic is wrong
-and can be simplified
+Status: Not the best possible solution. Come back to it later. Using hasttables/set is the correct approach but logic
+is wrong and can be simplified
 """
 from typing import List
 
 
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        ht_decreasing = {}
-        ht_increasing = {}
-        for num in nums:
-            if num in ht_decreasing:
-                lng = ht_decreasing.pop(num)
-                ht_decreasing[num-1] = max(lng+1, ht_decreasing.get(num-1, 1))
-            else:
-                ht_decreasing[num - 1] = ht_decreasing.get(num-1, 1)
-            if num in ht_increasing:
-                lng = ht_increasing.pop(num)
-                ht_increasing[num+1] = max(lng+1, ht_increasing.get(num-1, 1))
-            else:
-                ht_increasing[num+1] = ht_increasing.get(num+1, 1)
-
-            print(ht_decreasing, ht_increasing)
-        print("----")
-        for num in nums[::-1]:
-            if num in ht_decreasing:
-                lng = ht_decreasing.pop(num)
-                ht_decreasing[num-1] = max(lng+1, ht_decreasing.get(num-1, 1))
-            else:
-                ht_decreasing[num - 1] = ht_decreasing.get(num-1, 0) + 1
-            if num in ht_increasing:
-                lng = ht_increasing.pop(num)
-                ht_increasing[num+1] = max(lng+1, ht_increasing.get(num-1, 1))
-            else:
-                ht_increasing[num+1] = ht_increasing.get(num+1, 0) + 1
-
-            print(ht_decreasing, ht_increasing)
-        return max(max(ht_increasing.values())-1, max(ht_decreasing.values())-1)
+        if not nums:
+            return 0
+        nums.sort()
+        n = len(nums)
+        dp_arr = [1] * n
+        _max = 1
+        for i in range(1, n):
+            if nums[i] == nums[i - 1] + 1:
+                dp_arr[i] = dp_arr[i - 1] + 1
+            if nums[i] == nums[i - 1]:
+                dp_arr[i] = dp_arr[i - 1]
+        return max(dp_arr)
 
 
 s = Solution()
